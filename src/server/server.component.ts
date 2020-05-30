3c33175aa15fae4f52bc702dbdeb537726b6568b
Isra Ragheb <israragheb@gmail.com>
2020-05-30 09:34:46 +0200

    created: new server component

---
+++ src/server/server.component.ts
@@ -0,0 +1,14 @@
+// import from angular so that ts can uderstand that this is a component
+import {Component} from '@angular/core';
+
+
+@Component({
+    // this is the selector to be used in the hrml file
+    selector:'ap-server',
+    // this is the html file that will represent the component
+    templateUrl: './server.component.html'
+})
+
+export class ServerComponent{
+
+}
