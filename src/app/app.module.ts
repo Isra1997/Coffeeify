d51fc07a8f1e45176bd4eee536db445cea553469
Isra Ragheb <israragheb@gmail.com>
2020-06-24 15:49:50 +0200

    Added: Nav-bar and footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { CoffeeeBlockComponent } from './coffeee-block/coffeee-block.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
 
     NavBarComponent,
-    CoffeeeBlockComponent
+    CoffeeeBlockComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
